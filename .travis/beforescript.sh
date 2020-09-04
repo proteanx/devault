02d869ea31a2edf8ebe43f3a50d1e5da14a8ee90
Proteus <pro@proteanx.com>
2020-09-04 12:03:08 -0400

    try kitware

--- .travis/beforescript.sh
+++ .travis/beforescript.sh
@@ -8,10 +8,9 @@ mkdir -p depends/SDKs depends/sdk-sources
 
 if [[ $HOST = x86_64-apple-darwin14 ]]; then
   DOCKER_EXEC apt-get update
-  DOCKER_EXEC apt-get install software-properties-common -y
-  DOCKER_EXEC add-apt-repository ppa:hnakamur/libarchive -y
-  DOCKER_EXEC add-apt-repository ppa:hnakamur/libzstd -y
-  DOCKER_EXEC add-apt-repository ppa:hnakamur/cmake -y
+  DOCKER_EXEC apt-get install software-properties-common gnupg -y
+  wget -qO - https://apt.kitware.com/keys/kitware-archive-latest.asc |     DOCKER_EXEC apt-key add -
+  DOCKER_EXEC apt-add-repository 'deb https://apt.kitware.com/ubuntu/ bionic main'
 fi
 
 if [ -n "$OSX_SDK" -a ! -f depends/sdk-sources/MacOSX${OSX_SDK}.sdk.tar.gz ]; then
